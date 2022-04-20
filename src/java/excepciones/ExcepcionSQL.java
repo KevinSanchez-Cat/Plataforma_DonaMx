e8848bf190c2d3a510a46c04df6cb4043fc9c39c
KevinSanchez-Cat <por_salon@hotmail.com>
2022-04-19 21:07:06 -0500

    Excepcion Propia

---
+++ src/java/excepciones/ExcepcionSQL.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones;
+
+/**
+ *
+ * @author por_s
+ */
+public class ExcepcionSQL extends Exception {
+
+    public ExcepcionSQL(String msj) {
+        super(msj);
+    }
+    
+    
+    /*
+    
+    public void metodo() throws ExcepcionSQL
+    {
+        if(---){
+    
+        }else{
+            throw new ExcepcionSQL("MENSAJE DE ERROR")
+        }
+    }
+    
+    
+    try{
+    }catch(ExcepciobSQL ex){
+        sout(ex.getMessage())
+    }
+    
+    
+    */
+}
